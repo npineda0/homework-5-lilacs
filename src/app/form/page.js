dc8a3fdad364eab83aeed042e1f6070580aee667
Denisse <denissehenriquez@hotmail.com>
2024-11-13 14:05:05 -0500

    Finish adding the content for form page

--- src/app/form/page.js
+++ src/app/form/page.js
@@ -1,3 +1,29 @@
+"use client";
+
 export default function Form() {
-  return <h1>Form</h1>;
+  return (
+    <main>
+      <h1>Form</h1>
+      <p>Please submit any fun facts you have about Lilacs!</p>
+
+      <form>
+        <div>
+          <label>Title</label>
+          <input></input>
+        </div>
+        <div>
+          <label>Description</label>
+          <input></input>
+        </div>
+
+        <div>
+          <label>Image</label>
+          <input></input>
+        </div>
+        <div>
+          <input type="submit"></input>
+        </div>
+      </form>
+    </main>
+  );
 }
