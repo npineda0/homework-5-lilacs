8fbb1eba1864fb21239e34ead0b5963e353f095c
Denisse <denissehenriquez@hotmail.com>
2024-11-13 16:27:12 -0500

    Finish the read more for fun fact 3

---
+++ src/app/facts/factThree/page.js
@@ -0,0 +1,16 @@
+import threeStyle from "./three.module.css";
+
+export default function factThree() {
+  return (
+    <main className={threeStyle.threeMain}>
+      <h1>Lilacs Are Relics of Old Homesteads</h1>
+      <p>
+        It is use as a poetic expression as a sense of nostalgia and the passing
+        of time. Lilacs are known for their hardy nature and long life. Which
+        they can live for over a 100 years. Since they live for a long time, if
+        you are on a country road and see random lilac bush it is likely that a
+        house or farm was once there.
+      </p>
+    </main>
+  );
+}
