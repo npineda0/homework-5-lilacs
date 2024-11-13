01c9795573985cf32abbe8c96303ba68aacb9728
Denisse <denissehenriquez@hotmail.com>
2024-11-13 16:48:48 -0500

    finish the greek mythology page with styling

--- src/app/greek/page.js
+++ src/app/greek/page.js
@@ -1,7 +1,26 @@
+import greekStyle from "./greek.module.css";
+
 export default function greek() {
   return (
-    <main>
+    <main className={greekStyle.greekMain}>
       <h1>The Story of Pan</h1>
+      <p>Pan was the god of the forests and fields while Syringa was a nymph</p>
+      <p>
+        Pan was in love with Syringa but Syringa did not love Pan and was afraid
+        of Pan
+      </p>
+      <p>
+        Pan decides to follow Syringa and Syringa got scared, which lead her to
+        transform into a lilac bush
+      </p>
+      <p>
+        Pan found the lilac bush and decides to break a pieces of the bush which
+        he then regretted
+      </p>
+      <div>
+        <img src="https://c4israel.com.au/wp-content/uploads/Pan_God_of_the_wilderness_Greek_Mythology.jpg" />
+        <img src="https://static.wixstatic.com/media/365872_c771b9d1d3db4196accb001a802ad24e~mv2.jpg/v1/fill/w_828,h_1019,al_c,q_85,enc_auto/365872_c771b9d1d3db4196accb001a802ad24e~mv2.jpg" />
+      </div>
     </main>
   );
 }
