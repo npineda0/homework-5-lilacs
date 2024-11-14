d5a524f2a8625737d6c14e1a7c27529548ede6d2
fralcala <fralcala@iu.edu>
2024-11-13 21:49:55 -0500

    Set up and styled gallery page and uses page

--- src/app/uses/page.js
+++ src/app/uses/page.js
@@ -3,9 +3,21 @@ import styles from "./uses.module.css";
 export default function Uses() {
   return (
     <main className={styles.mainUses}>
-      <h1>Uses</h1>
-      {/* create slug to go to page about specific use */}
-      <p>uses text</p>
+      <div>
+        <h1>Uses</h1>
+        {/* create slug to go to page about specific use */}
+        <p>
+          landscaping, lilacs add vibrant color and lushness to gardens,
+          attracting bees and butterflies with their fragrant blossoms. Their
+          aromatic flowers are used to make essential oils, perfumes, and
+          scented candles. Lilac blooms can also be used in natural skincare
+          products for their soothing properties. Culinary enthusiasts use lilac
+          flowers to infuse syrups, teas, and baked goods, adding a mild floral
+          taste. Lilac wood is used for small woodworking projects like carving,
+          crafting decorative items, or making tool handles. Ornamental
+        </p>
+        <img src="/syrup.jpg" />
+      </div>
     </main>
   );
 }
